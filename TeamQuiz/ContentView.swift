719f6928b1b73e97220e127d247c730acebea911
김도윤 <kdy0096@gmail.com>
2026-04-14 00:59:26 +0900

    Initial Commit

---
+++ TeamQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeamQuiz
+//
+//  Created by yoonie on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeamQuiz/TeamQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamQuizApp.swift
+//  TeamQuiz
+//
+//  Created by yoonie on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct TeamQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
